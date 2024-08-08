12e494083b6f5b87a796839a09dbfa8f26368adb
danielsrod <coeng.daniel@gmail.com>
2024-08-08 00:26:40 -0400

    carrousel done (obs: Autoplay workaround)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,59 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
-export default function Home() {
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+	type CarouselOptions,
+} from "@/components/ui/carousel";
+
+const infos = [
+	{
+		id: 1,
+		ds_info: "My fullname is Daniel Sabino Rodrigues",
+	},
+	{
+		id: 2,
+		ds_info: "I was a professional footballer",
+	},
+	{
+		id: 3,
+		ds_info: "Actually, I'm good at barbecue",
+	},
+];
+
+const carrouselOpts: CarouselOptions = {
+	loop: true,
+};
+
+export default async function Home() {
 	return (
 		<main className="w-full h-[100vh] bg-zinc-500 flex row p-8 justify-around items-center">
-			<div className="w-[60%]">
-				<p>slide or carrousel apresentation</p>
+			<div className="w-[60%] flex justify-center">
+				<Carousel className="w-full max-w-xs" opts={carrouselOpts}>
+					<CarouselContent>
+						{infos.map((info) => (
+							<CarouselItem key={info.id}>
+								<div className="p-1">
+									<Card className="bg-transparent border-none shadow-none">
+										<CardContent className="flex aspect-square items-center justify-center p-6">
+											<span className="text-4xl font-semibold">
+												{info.ds_info}
+											</span>
+										</CardContent>
+									</Card>
+								</div>
+							</CarouselItem>
+						))}
+					</CarouselContent>
+					<CarouselPrevious variant={"ghost"} />
+					<CarouselNext variant={"ghost"} />
+				</Carousel>
 			</div>
-			<div className="w-[40%] h-96 flex justify-start">
+			<div className="w-[40%] h-96 flex justify-center">
 				<Tabs defaultValue="sign-in" className="w-[350px]">
 					<TabsList className="grid w-full grid-cols-2">
 						<TabsTrigger value="sign-in">Sign in</TabsTrigger>
@@ -42,7 +88,7 @@ export default function Home() {
 										</div>
 										<div className="flex flex-col space-y-1.5">
 											<Label htmlFor="password">Password</Label>
-											<Input id="password" />
+											<Input id="password" type="password" />
 										</div>
 									</div>
 								</form>
@@ -62,7 +108,7 @@ export default function Home() {
 									<div className="grid w-full items-center gap-4">
 										<div className="flex flex-col space-y-1.5">
 											<Label htmlFor="emailaddress">Email address</Label>
-											<Input id="emailaddress" />
+											<Input id="emailaddress" type="email" />
 										</div>
 									</div>
 								</form>
